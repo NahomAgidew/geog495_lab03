adcff64cd8536081f7cbf969bb34aba2e78cdae7
Nahom Abi <nahomagidew@gmail.com>
2021-10-29 15:05:07 -0700

    custom index html

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmFob21hIiwiYSI6ImNrdmN1M2VqdjNwc2MycW1zcGdxN
 
 let map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-  zoom: 5.5, // starting zoom
-  center: [138, 38] // starting center
+  style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+  zoom: 8, // starting zoom
+  center: [-123.690, 48.577] // starting center
 });
 
 let btn = document.getElementsByTagName("button")[0];
@@ -48,65 +48,62 @@ function sortTable(e) {
 }
 
 async function geojsonFetch() {
-  let response, earthquakes, japan, table;
-  response = await fetch('assets/earthquakes.geojson');
-  earthquakes = await response.json();
-  response = await fetch('assets/japan.json');
-  japan = await response.json();
+  let response, tremors, washington_bc, table;
+  response = await fetch('assets/tremors.json');
+  tremors = await response.json();
+  response = await fetch('assets/washington-bc.geojson');
+  washington_bc = await response.json();
 
   //load data to the map as new layers and table on the side.
   map.on('load', function loadingData() {
 
-    map.addSource('earthquakes', {
-        type: 'geojson',
-        data: earthquakes
+    map.addSource('tremors', {
+      type: 'geojson',
+      data: tremors
     });
 
     map.addLayer({
-        'id': 'earthquakes-layer',
-        'type': 'circle',
-        'source': 'earthquakes',
-        'paint': {
-            'circle-radius': 8,
-            'circle-stroke-width': 2,
-            'circle-color': 'red',
-            'circle-stroke-color': 'white'
-        }
+      'id': 'tremors-layer',
+      'type': 'circle',
+      'source': 'tremors',
+      'paint': {
+        'circle-radius': 4,
+        'circle-stroke-width': 2,
+        'circle-color': 'red',
+        'circle-stroke-color': 'white'
+      }
     });
 
 
-    map.addSource('japan', {
-        type: 'geojson',
-        data: japan
+    map.addSource('washington_bc', {
+      type: 'geojson',
+      data: washington_bc
     });
 
     map.addLayer({
-        'id': 'japan-layer',
-        'type': 'fill',
-        'source': 'japan',
-        'paint': {
-            'fill-color': '#0080ff', // blue color fill
-            'fill-opacity': 0.5
-        }
+      'id': 'washington_bc-layer',
+      'type': 'fill',
+      'source': 'washington_bc',
+      'paint': {
+        'fill-color': '#CBC3E3', // blue color fill
+        'fill-opacity': 0.5
+      }
     });
 
   });
 
   table = document.getElementsByTagName("table")[0];
   let row, cell1, cell2, cell3;
-  for (let i = 0; i < earthquakes.features.length; i++) {
-      // Create an empty <tr> element and add it to the 1st position of the table:
-      row = table.insertRow(-1);
-      cell1 = row.insertCell(0);
-      cell2 = row.insertCell(1);
-      cell3 = row.insertCell(2);
-      cell1.innerHTML = earthquakes.features[i].properties.id;
-      cell2.innerHTML = earthquakes.features[i].properties.mag;
-      cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
-          "en-US");
+  for (let i = 0; i < tremors.features.length; i++) {
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    row = table.insertRow(-1);
+    cell1 = row.insertCell(0);
+    cell2 = row.insertCell(1);
+    cell3 = row.insertCell(2);
+    cell1.innerHTML = tremors.features[i].properties.id;
+    cell2.innerHTML = tremors.features[i].properties.magnitude;
+    cell3.innerHTML = new Date(tremors.features[i].properties.time);
   }
-
-
 };
 
 geojsonFetch();
